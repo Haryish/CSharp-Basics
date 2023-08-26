abf5c9fbfaf1014cf65875d691085b698c9bcc5b
Haryish <haryishkumaran16@outlook.com>
2023-08-26 16:22:55 +0530

    Created Hello World Program

--- C# Fundamentals/Program.cs
+++ C# Fundamentals/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+
+//Printing Hello World
+
 namespace CSharpBasics
 {
     class PrintingStatements
     {
         static void Main(string[] args)
-        {
+        
+
             Console.WriteLine("Hello World::");
             Console.WriteLine("Please Run this program!!");
         }
