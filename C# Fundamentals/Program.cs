f7379fc81dddacd525a2095b1e5bfde0b9471450
Haryish <haryishkumaran16@outlook.com>
2023-08-26 16:23:49 +0530

    Hello WOrld Program

--- C# Fundamentals/Program.cs
+++ C# Fundamentals/Program.cs
@@ -7,8 +7,7 @@ namespace CSharpBasics
     class PrintingStatements
     {
         static void Main(string[] args)
-        
-
+        {
             Console.WriteLine("Hello World::");
             Console.WriteLine("Please Run this program!!");
         }
